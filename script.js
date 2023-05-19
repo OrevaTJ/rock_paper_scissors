edd727c533391d30cdca2aae7d74dbe1c2c23536
Oreva <khemical51@gmail.com>
2023-05-19 18:53:17 +0100

    Get game working by clicking buttons, no updating scores yet

--- script.js
+++ script.js
@@ -1,68 +1,49 @@
+let scores = document.querySelector('#scores')
+let score = Array.from(document.querySelectorAll('.score'));
+let playerScore = Number(score[0].textContent)
+console.log(playerScore + 1)
+let compScore = Number(score[1].textContent)
+console.log(compScore + 3)
 
-// let list = ["rock", "paper", "scissors" ]
 
-// function getComputerChoice() {
-//     return (list[Math.floor(Math.random() * list.length)])
-// }
-// const computerSelection = getComputerChoice();
-
-// const playerSelection = prompt("Pick Rock, Paper or Scissors").toLowerCase();
-// console.log(playRound(playerSelection, computerSelection))
-
-let score = [0, 0]
-
+// Play round compare player choice with computer choice
 function playRound(player, computer) {
     let message = `You win! ${player} beats ${computer}`
     if (player === "rock" && computer === "scissors" 
     || player === "scissors" && computer === "paper"
     || player === "paper" && computer === "rock"){
-        score[0] += 1
+        playerScore += 1
         console.log(message)
     } else if (player === computer){
         score = score
         console.log("A Tie")
     } else {
-        score[1] += 1
+        compScore += 1
         console.log(`You Lose! ${computer} beats ${player}`)
     }
 }
 
-function game() {
-    for (let i = 1; i <=5; i++) {
+// Get computer choice and call playRound with player selection
+function game(e) {
+    function getComputerChoice() {
         let list = ["rock", "paper", "scissors" ]
-
-        function getComputerChoice() {
-            return (list[Math.floor(Math.random() * list.length)])
-        }
-        const computerSelection = getComputerChoice();
-
-        const playerSelection = prompt("Pick Rock, Paper or Scissors").toLowerCase();
-        console.log(playRound(playerSelection, computerSelection))
+        return (list[Math.floor(Math.random() * list.length)])
     }
+    const computerSelection = getComputerChoice();
+    console.log(playRound(e.target.textContent.toLowerCase(), computerSelection))
 }
 
-// function record() {
-//     if (player === "rock" && computer === "scissors" 
-//     || player === "scissors" && computer === "paper"
-//     || player === "paper" && computer === "rock") {
-//         score[0] += 1
-//     } else if (player === computer){
-//         score = score
-//     } else {
-//         score[1] += 1
-//     }
-// }
+// Add EventListeners to R/P/S buttons
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
-game()
-console.log(score)
-
-if (score[0] > score[1]) {
-    console.log( "Player Defeats Computer")
-} else if (score[0] === score[1]) {
-    console.log("A Draw Game")
-} else {
-    console.log("Computer Defeats Player")
-}
-
-
-
+rock.addEventListener('click', function(e) {
+    game(e)
+})
+paper.addEventListener('click', function(e) {
+    game(e)
+})
+scissors.addEventListener('click', function(e) {
+    game(e)
+})
